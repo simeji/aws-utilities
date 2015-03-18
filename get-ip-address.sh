976d86ef6c766fc19527a4804981823d9a5c25a9
simeji <simeji.net@gmail.com>
2015-03-18 21:34:18 +0900

    add jq filter

--- get-ip-address.sh
+++ get-ip-address.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+(test -z $1 || test -z $2) && echo "require {NameTag}, {Profile}" && exit 1
 NAME=$1
 PROFILE=$2
 OTHER=$3
-aws ec2 describe-instances --filter "Name=tag-key,Values=Name" "Name=tag-value,Values=$NAME" --query 'Reservations[].Instances[?State.Name==`running`].PrivateIpAddress[]' --profile $2 $3
+aws ec2 describe-instances --filter "Name=tag-key,Values=Name" "Name=tag-value,Values=$NAME" --query 'Reservations[].Instances[?State.Name==`running`].PrivateIpAddress[]' --profile $2 $3 | jq -r '.[]'
